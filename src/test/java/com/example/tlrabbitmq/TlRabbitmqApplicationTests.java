2a6f40d5cc684c71562dcdecc3e23d692de41eb8
wangjing1995 <32173782+wangjing1995@users.noreply.github.com>
2021-04-10 13:58:55 +0800

    Initial commit

---
+++ src/test/java/com/example/tlrabbitmq/TlRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tlrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
